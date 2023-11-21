6eb9cab972faf6084d389fb4835645ce3e3cd8e6
tolgagolet <m.tolgaglt@gmail.com>
2023-11-22 00:03:12 +0300

    added mantine uıi integration

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,21 +1,123 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { useDisclosure } from "@mantine/hooks";
+import {
+  AppShell,
+  Burger,
+  Group,
+  Title,
+  UnstyledButton,
+  useMantineColorScheme,
+  useComputedColorScheme,
+  ActionIcon,
+  Button,
+} from "@mantine/core";
 import Header from "../components/Header";
+import classes from "./RootLayout.module.css";
+import { IconSun, IconMoon } from "@tabler/icons-react";
+import cx from "clsx";
 
 export default function RootLayout() {
+  const [opened, { toggle }] = useDisclosure();
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
+
   return (
     <div className="root-layout">
-      <header>
-        <nav>
-          <h1>MTG Wallet</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="careers">Careers</NavLink>
-        </nav>
-      </header>
-      <p>AuthHeader:</p>
-      <Header></Header>
-      <main>
-        <Outlet />
-      </main>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { desktop: true, mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            <Group justify="space-between" style={{ flex: 1 }}>
+              <Title order={1} className={classes.title}>
+                MTG Wallet
+              </Title>
+              <Group ml="xl" gap={0} visibleFrom="sm">
+                <NavLink to="/" className={classes.control}>
+                  <UnstyledButton>Home</UnstyledButton>
+                </NavLink>
+                <NavLink to="/careers" className={classes.control}>
+                  <UnstyledButton>Careers</UnstyledButton>
+                </NavLink>
+                <ActionIcon
+                  onClick={() =>
+                    setColorScheme(
+                      computedColorScheme === "light" ? "dark" : "light"
+                    )
+                  }
+                  variant="default"
+                  size="xl"
+                  aria-label="Toggle color scheme"
+                >
+                  {computedColorScheme === "light" ? (
+                    <IconMoon
+                      className={cx(classes.icon, classes.dark)}
+                      stroke={1.5}
+                    />
+                  ) : (
+                    <IconSun
+                      className={cx(classes.icon, classes.light)}
+                      stroke={1.5}
+                    />
+                  )}
+                </ActionIcon>
+              </Group>
+            </Group>
+          </Group>
+        </AppShell.Header>
+
+        <AppShell.Navbar py="md" px={4}>
+          <NavLink to="/" className={classes.control}>
+            <UnstyledButton onClick={toggle}>Home</UnstyledButton>
+          </NavLink>
+          <NavLink to="/careers" className={classes.control}>
+            <UnstyledButton onClick={toggle}>Careers</UnstyledButton>
+          </NavLink>
+          <Button
+            onClick={() =>
+              setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+            }
+            justify="center"
+            fullWidth
+            leftSection={
+              computedColorScheme === "light" ? (
+                <IconMoon
+                  className={cx(classes.icon, classes.dark)}
+                  stroke={1.5}
+                />
+              ) : (
+                <IconSun
+                  className={cx(classes.icon, classes.light)}
+                  stroke={1.5}
+                />
+              )
+            }
+            variant="default"
+            mt="md"
+          >
+            Toggle Dark / Light Mode
+          </Button>
+        </AppShell.Navbar>
+
+        <AppShell.Main>
+          <Header />
+          <Outlet />
+        </AppShell.Main>
+      </AppShell>
     </div>
   );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,15 @@
 import React, { useContext, useEffect } from "react";
 import AuthContext from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import {
+  TextInput,
+  PasswordInput,
+  Anchor,
+  Paper,
+  Container,
+  Group,
+  Button,
+} from "@mantine/core";
 
 const LoginPage = () => {
   let { loginUser, user } = useContext(AuthContext);
@@ -19,6 +28,25 @@ const LoginPage = () => {
         <input type="password" name="password" placeholder="Enter Password" />
         <input type="submit" />
       </form>
+      <Container size={420} my={40}>
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <TextInput label="Username" placeholder="username" required />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            required
+            mt="md"
+          />
+          <Group justify="space-between" mt="lg">
+            <Anchor component="button" size="sm">
+              Forgot password?
+            </Anchor>
+          </Group>
+          <Button fullWidth mt="xl">
+            Sign in
+          </Button>
+        </Paper>
+      </Container>
     </div>
   );
 };
