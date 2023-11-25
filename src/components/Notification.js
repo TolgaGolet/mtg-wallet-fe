555816d8ce5bb4673eaf787f7402e111dffe7b34
tolgagolet <m.tolgaglt@gmail.com>
2023-11-25 21:30:48 +0300

    added notifications system, updated login page

---
+++ src/components/Notification.js
@@ -0,0 +1,36 @@
+import { notifications } from "@mantine/notifications";
+import { rem } from "@mantine/core";
+import { IconX, IconCheck } from "@tabler/icons-react";
+
+const Notification = (message, type) => {
+  const autoCloseDuration = 5000;
+
+  let notificationConfig;
+
+  if (type === "success") {
+    notificationConfig = {
+      color: "teal",
+      title: "Success",
+      message: message,
+      icon: <IconCheck style={{ width: rem(18), height: rem(18) }} />,
+      autoClose: autoCloseDuration,
+    };
+  } else if (type === "error") {
+    notificationConfig = {
+      color: "red",
+      title: "Error",
+      message: message,
+      icon: <IconX style={{ width: rem(18), height: rem(18) }} />,
+      autoClose: autoCloseDuration,
+    };
+  } else {
+    notificationConfig = {
+      message: message,
+      autoClose: autoCloseDuration,
+    };
+  }
+
+  return <>{notifications.show(notificationConfig)}</>;
+};
+
+export default Notification;
